48f70bf3d8a0487ef3eb885dd5970a4b10f19c57
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-07-06 01:55:45 -0300

    adicionando classe User

---
+++ src/Models/User.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models;
+
+use App\Core\Database;
+
+class User
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+    public function create($data)
+    {
+        $stmt = $this->db->prepare(
+            "INSERT INTO users (name, email, password) 
+            VALUES (:name, :email, :password)"
+        );
+        return $stmt->execute($data);
+    }
+
+    public function findByEmail($email)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        return $stmt->fetch();
+    }
+}
